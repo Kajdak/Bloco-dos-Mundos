49edcf2baed8ef147c11ea90ed808d2ff84875f6
Kajdak <erick_alankramer@hotmail.com>
2020-03-09 13:45:03 +0000

    cu

--- Main.py
+++ Main.py
@@ -1,9 +1,23 @@
 import random
 
-class Broco:
+class Bloco:
     blocos = ['A','B','C']
     pilha1 = []
     pilha2 = []
     pilha3 = []
 
-    def init()
+    def init(self):
+        while(self.blocos):
+            i=random.randint(0,2)
+            aux=self.blocos.pop(i)
+            i=random.randint(1,3)
+            if(i==1):
+                self.pilha1.append(aux)
+            elif(i==2):
+                self.pilha2.append(aux)
+            elif(i==3):
+                self.pilha3.append(aux)
+            print(self.pilha1,self.pilha2,self.pilha3)
+
+bl = Bloco()
+bl.init()
